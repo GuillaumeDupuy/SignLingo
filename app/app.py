c28d1e0d2b0de029c0eb9da9093cd850a433b33c
Charles Fisman <fismanc@gmail.com>
2024-04-16 16:01:22 +0200

    revert cal

--- app/app.py
+++ app/app.py
@@ -189,17 +189,7 @@ st.write("### Record your sign language video:")
 #     async_processing=False,
 # )
 
-import av
-
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    flipped = img[::-1,:,:]
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example")
 
 # col1, col2 = st.columns([1,1])
 
