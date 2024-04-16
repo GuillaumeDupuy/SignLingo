0ad425750ff8e4b7c4dc7b0043c3aedd37001bc9
Guillaume <guillaume.dupuy@ynov.com>
2024-04-16 11:24:43 +0200

    add translate streamlit

--- app/test.py
+++ app/test.py
@@ -7,9 +7,15 @@ import csv
 import numpy as np
 from collections import deque, Counter
 import copy
+from gtts import gTTS, lang
+from googletrans import Translator, LANGUAGES
 from utils import CvFpsCalc
 from model import KeyPointClassifier
 from model import PointHistoryClassifier
+
+# ---------------------------------------------------------------------------------------------------------------
+# Fonctions Model
+# ---------------------------------------------------------------------------------------------------------------
 
 def calc_bounding_rect(image, landmarks):
     image_width, image_height = image.shape[1], image.shape[0]
@@ -296,6 +302,8 @@ def draw_info_text(image, brect, handedness, hand_sign_text,
         info_text = info_text + ':' + hand_sign_text
     cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
                cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
+    
+    # st.write("Hand Gesture:", hand_sign_text)
 
     if finger_gesture_text != "":
         cv.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 60),
@@ -331,12 +339,169 @@ def draw_info(image, fps, mode, number):
                        cv.LINE_AA)
     return image
 
+# ---------------------------------------------------------------------------------------------------------------
+# Fonctions Trad
+# ---------------------------------------------------------------------------------------------------------------
+
+def detect_lang(texte):
+    """
+    Detect the language of the text
+    """
+    detect = Translator()
+    detect_lang = detect.detect(texte)
+    return detect_lang.lang
+
+def translate_text(texte, langue_cible):
+    """
+    Translate the text to the target language
+    """
+    translator = Translator()
+    traduction = translator.translate(texte, dest=langue_cible)
+    return traduction.text
+
+def text_to_audio(texte, langue):
+    """
+    Convert the text to speech
+    """
+    tts = gTTS(text=texte, lang=langue, slow=False)
+    tts.save("output.mp3")
+
 def main():
-    st.title("Hand Gesture Recognition")
+# ---------------------------------------------------------------------------------------------------------------
+# Page configuration
+# ---------------------------------------------------------------------------------------------------------------
+    st.set_page_config(
+        page_title="SignLingo", page_icon="👋", initial_sidebar_state="expanded"
+    )
+
+    global hand_text
+    hand_text = "Waiting, with a word of hand"
+
+# ---------------------------------------------------------------------------------------------------------------
+# Page layout
+# ---------------------------------------------------------------------------------------------------------------
+    st.title("SignLingo")
+
+    st.write('<br>', unsafe_allow_html=True)
+    st.write("### SignLingo is a web app that translates sign language to text & speech.")
+    st.write('#### Bridging Silence with Words and a World where Every Sign Counts')
+    st.write('<br>', unsafe_allow_html=True)
+
+    list_langues = {
+        'af': 'afrikaans',
+        'sq': 'albanian',
+        'am': 'amharic',
+        'ar': 'arabic',
+        'hy': 'armenian',
+        'az': 'azerbaijani',
+        'be': 'belarusian',
+        'bn': 'bengali',
+        'bs': 'bosnian',
+        'bg': 'bulgarian',
+        'ca': 'catalan',
+        'ceb': 'cebuano',
+        'ny': 'chichewa',
+        'zh-cn': 'chinese (simplified)',
+        'zh-tw': 'chinese (traditional)',
+        'hr': 'croatian',
+        'cs': 'czech',
+        'da': 'danish',
+        'nl': 'dutch',
+        'en': 'english',
+        'eo': 'esperanto',
+        'et': 'estonian',
+        'tl': 'filipino',
+        'fi': 'finnish',
+        'fr': 'french',
+        'fy': 'frisian',
+        'gl': 'galician',
+        'ka': 'georgian',
+        'de': 'german',
+        'el': 'greek',
+        'gu': 'gujarati',
+        'ht': 'haitian creole',
+        'ha': 'hausa',
+        'haw': 'hawaiian',
+        'iw': 'hebrew',
+        'hi': 'hindi',
+        'hmn': 'hmong',
+        'hu': 'hungarian',
+        'is': 'icelandic',
+        'ig': 'igbo',
+        'id': 'indonesian',
+        'ga': 'irish',
+        'it': 'italian',
+        'ja': 'japanese',
+        'jw': 'javanese',
+        'kn': 'kannada',
+        'kk': 'kazakh',
+        'km': 'khmer',
+        'ko': 'korean',
+        'ku': 'kurdish (kurmanji)',
+        'ky': 'kyrgyz',
+        'lo': 'lao',
+        'la': 'latin',
+        'lv': 'latvian',
+        'lt': 'lithuanian',
+        'lb': 'luxembourgish',
+        'mk': 'macedonian',
+        'mg': 'malagasy',
+        'ms': 'malay',
+        'ml': 'malayalam',
+        'mt': 'maltese',
+        'mi': 'maori',
+        'mr': 'marathi',
+        'mn': 'mongolian',
+        'my': 'myanmar (burmese)',
+        'ne': 'nepali',
+        'no': 'norwegian',
+        'ps': 'pashto',
+        'fa': 'persian',
+        'pl': 'polish',
+        'pt': 'portuguese',
+        'pa': 'punjabi',
+        'ro': 'romanian',
+        'ru': 'russian',
+        'sm': 'samoan',
+        'gd': 'scots gaelic',
+        'sr': 'serbian',
+        'st': 'sesotho',
+        'sn': 'shona',
+        'sd': 'sindhi',
+        'si': 'sinhala',
+        'sk': 'slovak',
+        'sl': 'slovenian',
+        'so': 'somali',
+        'es': 'spanish',
+        'su': 'sundanese',
+        'sw': 'swahili',
+        'sv': 'swedish',
+        'tg': 'tajik',
+        'ta': 'tamil',
+        'te': 'telugu',
+        'th': 'thai',
+        'tr': 'turkish',
+        'uk': 'ukrainian',
+        'ur': 'urdu',
+        'ug': 'uyghur',
+        'uz': 'uzbek',
+        'vi': 'vietnamese',
+        'cy': 'welsh',
+        'xh': 'xhosa',
+        'yi': 'yiddish',
+        'yo': 'yoruba',
+        'zu': 'zulu',
+    }
+
+# ---------------------------------------------------------------------------------------------------------------
+# Page content
+# ---------------------------------------------------------------------------------------------------------------
+    st.write("### Record your sign language video:")
+
     run = st.checkbox('Run', key="run_checkbox")
     frame_window = st.image([], channels="BGR", width=640)
 
-    if run:
+    if run == True:
         cap_device = 0
         cap_width = 640
         cap_height = 480
@@ -370,8 +535,9 @@ def main():
         point_history = deque(maxlen=history_length)
         finger_gesture_history = deque(maxlen=history_length)
         mode = 0
-
         cvFpsCalc = CvFpsCalc(buffer_len=10)
+
+        stop_button = st.button('Stop', key="stop_button")
 
         while run:
             fps = cvFpsCalc.get()
@@ -393,7 +559,7 @@ def main():
                     pre_processed_landmark_list = pre_process_landmark(landmark_list)
                     pre_processed_point_history_list = pre_process_point_history(debug_image, point_history)
                     hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                    if hand_sign_id == 2:  # Point gesture
+                    if hand_sign_id == 2:
                         point_history.append(landmark_list[8])
                     else:
                         point_history.append([0, 0])
@@ -405,9 +571,11 @@ def main():
 
                     debug_image = draw_bounding_rect(True, debug_image, brect)
                     debug_image = draw_landmarks(debug_image, landmark_list)
-                    debug_image = draw_info_text(debug_image, brect, handedness,
-                                                keypoint_classifier_labels[hand_sign_id],
-                                                point_history_classifier_labels[most_common_fg_id[0][0]])
+                    debug_image = draw_info_text(debug_image, brect, handedness,keypoint_classifier_labels[hand_sign_id],point_history_classifier_labels[most_common_fg_id[0][0]])
+
+                    # st.write("Hand Gesture:", keypoint_classifier_labels[hand_sign_id])
+                    hand_text = keypoint_classifier_labels[hand_sign_id]
+
             else:
                 point_history.append([0, 0])
 
@@ -415,12 +583,50 @@ def main():
             debug_image = draw_info(debug_image, fps, mode, 0)
             frame_window.image(debug_image, channels="BGR")
 
-            # if st.button('Stop', key="stop_button"):
-            #     break
+            if stop_button:
+                run = False
+                break
 
         cap.release()  
     else:
-        st.write("Stopped or not started yet")
+        st.write('<br>', unsafe_allow_html=True)
+
+        st.write("### Translate your text:")
+
+        # texte = st.text_input("Type your text here :", "How are you ?")
+        texte = hand_text
+        texte = texte.lower()
+
+        st.write("### Your text is : " + texte)
+
+        lang_detect = detect_lang(texte)
+        # Recover the language name from the language code
+        langue_detect = list_langues[lang_detect]
+
+        st.write("### Your text is in " + langue_detect)
+
+        # Select the language to translate to (default: french)
+        st.write('<br>', unsafe_allow_html=True)
+        langue = st.selectbox("Select a language to translate to :", list(list_langues.values()), index=list(list_langues.values()).index('french'))
+        # Recover the language code from the language name
+        langue_code = list(list_langues.keys())[list(list_langues.values()).index(langue)]
+
+        texte_traduit = translate_text(texte, langue_code)
+
+        st.write("### Your text translated to " + langue + " is : " + texte_traduit)
+        st.write('<br>', unsafe_allow_html=True)
+
+
+        # Check if the selected language is supported by gTTS
+        if langue_code in lang.tts_langs():
+            st.write("### Your text translated to speech:")
+
+            text_to_audio(texte_traduit, langue_code)
+            audio_file = open('output.mp3', 'rb')
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/mp3')
+        else:
+            st.write("### Sorry, the selected language is not supported for text-to-speech.")
 
 if __name__ == "__main__":
     main()
